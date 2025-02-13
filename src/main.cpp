8ee1d6a05f896d10121134143d9fb725a624536a
Tomasz Sarkowicz <nox@noxsoftware.pl>
2025-02-13 22:02:02 +0100

    todo cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include "tokenizer.h"
 
 // TODO: use HIP for GPU acceleration
-// TODO: create a subword tokenizer
 // TODO: tokenize training and test datasets using subword tokenizer into training.tok and test.tok
 // TODO: use "Attention is All You Need" in GPT architecture
 // TODO: use Adam optimizer to train the model (gradient descent)
